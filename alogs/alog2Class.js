b5dcb9dee9eaa732795b8bba332186fe5887a2da
ejrhgk1022 <ejrhgk1022@gmail.com>
2025-12-05 07:49:42 +0000

    3차청산으로 수정

--- alogs/alog2Class.js
+++ alogs/alog2Class.js
@@ -28,18 +28,22 @@ class alogo2{
         this.openPrice = 0.0;
         this.exit_price_1 = 0.0
         this.exit_price_2 = 0.0
+        this.exit_price_3 = 0.0
 
         this.exit_size_1 = 0.0; //1차 청산 물량
         this.exit_size_2 = 0.0; //2차 청산 물량
+        this.exit_size_3 = 0.0; //3차 청산 물량
 
         this.orderId_open = null//오더링크아이디로 용
         this.orderId_exit_1 = null//오더링크아이디 용
         this.orderId_exit_2 = null//오더링크아이디 용
+        this.orderId_exit_3 = null//오더링크아이디 용
 
-        this.positionType = null;//long short null
+
         this.isOpenOrderFilled = false
-        this.isPartialExit = false;// 부분익절 여부
-
+        this.exit_count = 0;// 부분익절 여부
+        
+        this.positionType = null;//long short null
         this.entry_allow = false // adx20이상, 증가여부
 
         
@@ -63,7 +67,7 @@ class alogo2{
 
         if(data){
             Object.assign(this, data);
-            await this.doubleCheckStatus()
+            //await this.doubleCheckStatus()
             
         }
 
@@ -88,8 +92,10 @@ class alogo2{
         const adxObj2 = calculateDMI(data, 14, 2);//전전봉
 
         const alligatorObj = calculateAlligator(data, 0)
-        const ema_5 = alligatorObj.teeth//calculateEMA(data, 5, 0);
-        const ema_10 = alligatorObj.jaw//calculateEMA(data, 10, 0);
+
+        const lips_8 = alligatorObj.lips
+        const teeth_13 = alligatorObj.teeth//calculateEMA(data, 5, 0);
+        const jaw_21 = alligatorObj.jaw//calculateEMA(data, 10, 0);
 
         //adx 조건 계산
         if(adxObj.adx >20 && adxObj.adx > adxObj2.adx){
@@ -101,13 +107,13 @@ class alogo2{
         //포지션타입계산 
         if(current_open > bbObj.upper){
 
-            if(current_open > ema_5 && current_open > ema_10){
+            if(current_open > lips_8 && current_open > teeth_13 && current_open > jaw_21){
                 this.positionType = 'long'
             }           
 
         }else if(current_open < bbObj.lower){
             
-            if(current_open < ema_5 && current_open < ema_10){
+            if(current_open < lips_8 && current_open < teeth_13 && current_open < jaw_21){
                 this.positionType = 'short'
             }
 
@@ -121,11 +127,12 @@ class alogo2{
             return
         }
 
-        const rawOrderSize = this.calculatePositionSize(current_open, (ema_5 + ema_10) / 2);
+        const rawOrderSize = this.calculatePositionSize(current_open, (lips_8 + teeth_13 + jaw_21) / 3);
         this.orderSize = Math.round(rawOrderSize * this.qtyMultiplier) / this.qtyMultiplier;
         
-        this.exit_size_1 = Math.round((this.orderSize / 2) * this.qtyMultiplier) / this.qtyMultiplier;
-        this.exit_size_2 = Math.round((this.orderSize - this.exit_size_1) * this.qtyMultiplier) / this.qtyMultiplier;
+        this.exit_size_1 = Math.round((this.orderSize / 3) * this.qtyMultiplier) / this.qtyMultiplier;
+        this.exit_size_2 = Math.round((this.orderSize / 3) * this.qtyMultiplier) / this.qtyMultiplier;
+        this.exit_size_3 = Math.round((this.orderSize - this.exit_size_1 - this.exit_size_2) * this.qtyMultiplier) / this.qtyMultiplier;
 
         const side = this.positionType == 'long' ? 'Buy' : 'Sell'
 
@@ -160,30 +167,31 @@ class alogo2{
         const data = await getKline(this.symbol, '240', 200)
 
         const alligatorObj = calculateAlligator(data, 0)
-        let ema_5 = alligatorObj.teeth//calculateEMA(data, 5, 0);
-        let ema_10 = alligatorObj.jaw//calculateEMA(data, 10, 0);
 
-        ema_5 = Math.round(ema_5 * this.priceMultiplier) / this.priceMultiplier;
-        ema_10 = Math.round(ema_10 * this.priceMultiplier) / this.priceMultiplier;
+        let lips_8 = alligatorObj.lips
+        let teeth_13 = alligatorObj.teeth//calculateEMA(data, 5, 0);
+        let jaw_21 = alligatorObj.jaw//calculateEMA(data, 10, 0);
 
+        lips_8 =  Math.round(lips_8 * this.priceMultiplier) / this.priceMultiplier;
+        teeth_13 = Math.round(teeth_13 * this.priceMultiplier) / this.priceMultiplier;
+        jaw_21 = Math.round(jaw_21 * this.priceMultiplier) / this.priceMultiplier;
 
-        if(this.positionType == "long" && ema_5 > ema_10){
-            this.exit_price_1 = ema_5
-            this.exit_price_2 = ema_10
-        }
-        else if(this.positionType == "long" && ema_5 < ema_10){
-            this.exit_price_1 = ema_10
-            this.exit_price_2 = ema_5
+        const prices = [lips_8, teeth_13, jaw_21];
+        // 포지션에 따라 정렬합니다.
+
+        if (this.positionType === 'long') {
+            // 롱 포지션: 내림차순 정렬 (높은 가격부터 청산)
+            prices.sort((a, b) => b - a);
+        } else { // 'short'
+            // 숏 포지션: 오름차순 정렬 (낮은 가격부터 청산)
+            prices.sort((a, b) => a - b);
         }
 
-        if(this.positionType == "short" && ema_5 < ema_10){
-            this.exit_price_1 = ema_5
-            this.exit_price_2 = ema_10
-        }
-        else if(this.positionType == "short" && ema_5 > ema_10){
-            this.exit_price_1 = ema_10
-            this.exit_price_2 = ema_5
-        }
+        // 정렬된 가격을 청산 가격으로 설정합니다.
+        this.exit_price_1 = prices[0];
+        this.exit_price_2 = prices[1];
+        this.exit_price_3 = prices[2]; // 3차 청산 가격
+
         
         const side = this.positionType == 'long' ? 'Sell' : 'Buy'
         const triggerDirection = this.positionType === 'long' ? '2' : '1'; // 1: Rise, 2: Fall
@@ -253,6 +261,41 @@ class alogo2{
         
             });
 
+            
+        const exit3Params = {
+            category: "linear",
+            symbol: this.symbol,
+            side: side,
+            qty: (this.exit_size_3).toString(),
+            triggerPrice: (this.exit_price_3).toString(),
+            triggerDirection: triggerDirection,
+            triggerBy: "MarkPrice",
+            orderType: "Market",
+            reduceOnly: true,
+            orderLinkId : this.orderId_exit_3,
+            timeInForce: "GoodTillCancel"
+        };
+        
+        consoleLogger.order(`${this.name} 3차 청산 설정`, exit3Params);
+        ws_client.sendWSAPIRequest(WS_KEY_MAP.v5PrivateTrade, 'order.create', exit3Params)
+            .catch((e) => {
+                fileLogger.error('order exit3 error:', e);
+                consoleLogger.error('order exit3 error:', e);
+                consoleLogger.error('order exit3 error 강제 청산 실행');
+                
+                const marketCloseParams = { ...exit3Params }; 
+                delete marketCloseParams.triggerPrice;
+                delete marketCloseParams.triggerDirection;
+                delete marketCloseParams.triggerBy;
+                delete marketCloseParams.timeInForce;
+
+                this.setNewOrderId()
+                marketCloseParams.orderLinkId = this.orderId_exit_3
+                ws_client.sendWSAPIRequest(WS_KEY_MAP.v5PrivateTrade, 'order.create', marketCloseParams)     
+        
+            });
+
+
     }
 
     async updateStop(){//포지션이있는경우 반복되어야함
@@ -260,31 +303,31 @@ class alogo2{
         const data = await getKline(this.symbol, '240', 200);
 
         const alligatorObj = calculateAlligator(data, 0)
-        let ema_5 = alligatorObj.teeth//calculateEMA(data, 5, 0);
-        let ema_10 = alligatorObj.jaw//calculateEMA(data, 10, 0);
+        let lips_8 = alligatorObj.lips
+        let teeth_13 = alligatorObj.teeth//calculateEMA(data, 5, 0);
+        let jaw_21 = alligatorObj.jaw//calculateEMA(data, 10, 0);
 
-        ema_5 = Math.round(ema_5 * this.priceMultiplier) / this.priceMultiplier;
-        ema_10 = Math.round(ema_10 * this.priceMultiplier) / this.priceMultiplier;
+        lips_8 =  Math.round(lips_8 * this.priceMultiplier) / this.priceMultiplier;
+        teeth_13 = Math.round(teeth_13 * this.priceMultiplier) / this.priceMultiplier;
+        jaw_21 = Math.round(jaw_21 * this.priceMultiplier) / this.priceMultiplier;
 
-        if(this.positionType == "long" && ema_5 > ema_10){
-            this.exit_price_1 = ema_5
-            this.exit_price_2 = ema_10
-        }
-        else if(this.positionType == "long" && ema_5 < ema_10){
-            this.exit_price_1 = ema_10
-            this.exit_price_2 = ema_5
+        const prices = [lips_8, teeth_13, jaw_21];
+        // 포지션에 따라 정렬
+
+        if (this.positionType === 'long') {
+            // 롱 포지션: 내림차순 정렬 (높은 가격부터 청산)
+            prices.sort((a, b) => b - a);
+        } else { // 'short'
+            // 숏 포지션: 오름차순 정렬 (낮은 가격부터 청산)
+            prices.sort((a, b) => a - b);
         }
 
-        if(this.positionType == "short" && ema_5 < ema_10){
-            this.exit_price_1 = ema_5
-            this.exit_price_2 = ema_10
-        }
-        else if(this.positionType == "short" && ema_5 > ema_10){
-            this.exit_price_1 = ema_10
-            this.exit_price_2 = ema_5
-        }
+        // 정렬된 가격을 청산 가격으로 설정
+        this.exit_price_1 = prices[0];
+        this.exit_price_2 = prices[1];
+        this.exit_price_3 = prices[2]; // 3차 청산 가격
         
-        if(this.isPartialExit == false){//부분익절안되어있는경우면 1차 스탑도 업데이트
+        if(this.exit_count < 1){//청산 횟수가 0일때
             
             const amend1Params = {
                 category: "linear",
@@ -301,20 +344,40 @@ class alogo2{
                 });
         }
 
-        const amend2Params = {
-            category: "linear",
-            symbol: this.symbol,
-            triggerPrice: (this.exit_price_2).toString(),
-            orderLinkId : this.orderId_exit_2,
-        };
-        
-        consoleLogger.order(`${this.name} 2차 청산 주문 수정 요청`, amend2Params);
-        ws_client.sendWSAPIRequest(WS_KEY_MAP.v5PrivateTrade, 'order.amend', amend2Params)
-            .catch((e) => {
-                fileLogger.error('amend exit2 error:', e);
-                consoleLogger.error('amend exit2 error:', e);
+        if(this.exit_count < 2){//청산 횟수가 0이거나 1일때
+            
+            const amend2Params = {
+                category: "linear",
+                symbol: this.symbol,
+                triggerPrice: (this.exit_price_2).toString(),
+                orderLinkId : this.orderId_exit_2,
+            };
+            
+            consoleLogger.order(`${this.name} 2차 청산 주문 수정 요청`, amend2Params);
+            ws_client.sendWSAPIRequest(WS_KEY_MAP.v5PrivateTrade, 'order.amend', amend2Params)
+                .catch((e) => {
+                    fileLogger.error('amend exit2 error:', e);
+                    consoleLogger.error('amend exit2 error:', e);
             });
+        }
 
+        
+        if(this.exit_count < 3){//청산 횟수가 0이거나 1이거나 2일때
+            
+            const amend3Params = {
+                category: "linear",
+                symbol: this.symbol,
+                triggerPrice: (this.exit_price_3).toString(),
+                orderLinkId : this.orderId_exit_3,
+            };
+            
+            consoleLogger.order(`${this.name} 3차 청산 주문 수정 요청`, amend3Params);
+            ws_client.sendWSAPIRequest(WS_KEY_MAP.v5PrivateTrade, 'order.amend', amend3Params)
+                .catch((e) => {
+                    fileLogger.error('amend exit3 error:', e);
+                    consoleLogger.error('amend exit3 error:', e);
+            });
+        }
 
     }
 
@@ -323,18 +386,20 @@ class alogo2{
         this.orderSize = 0.0
         this.exit_size_1 = 0.0
         this.exit_size_2 = 0.0
+        this.exit_size_3 = 0.0
 
         this.openPrice = 0.0
 
         this.positionType = null
 
         this.entry_allow = false
-        this.isPartialExit = false
+        this.exit_count = 0
         this.isOpenOrderFilled = false
 
         this.orderId_open = null
         this.orderId_exit_1 = null
         this.orderId_exit_2 = null
+        this.orderId_exit_3 = null
 
     }
 
@@ -348,12 +413,13 @@ class alogo2{
         const data = {...dataObj, 
             openPrice : this.openPrice, 
             exit_price_1 : this.exit_price_1, 
-            exit_price_2 : this.exit_price_2
+            exit_price_2 : this.exit_price_2,
+            exit_price_3 : this.exit_price_3
         }
         const tradeLogDocId = "algo2_"+this.symbol
         addTradeLog(tradeLogDocId,data)
 
-        consoleLogger.order(`${this.name} ${dataObj.orderLinkId} 체결 -- side: ${dataObj.side}, price: ${dataObj.price}, qty: ${dataObj.qty}, pnl:  ${dataObj.closedPnl}`);
+        consoleLogger.order(`${this.name} ${dataObj.orderLinkId} 체결 -- side: ${dataObj.side}, price: ${dataObj.price}, qty: ${dataObj.qty}, pnl: ${dataObj.closedPnl}`);
 
         if(this.orderId_open == dataObj.orderLinkId){
 
@@ -362,11 +428,15 @@ class alogo2{
         }
 
         if(this.orderId_exit_1 == dataObj.orderLinkId){
-            this.isPartialExit = true
+            this.exit_count ++
 
         }
 
         if(this.orderId_exit_2 == dataObj.orderLinkId){
+            this.exit_count ++
+        }
+
+        if(this.orderId_exit_3 == dataObj.orderLinkId){
             this.reset()
         }
 
@@ -382,49 +452,50 @@ class alogo2{
         return docId
     }
 
-    async doubleCheckStatus(){
-        const res1 = await rest_client.getActiveOrders({ 
-            category: 'linear',
-            symbol: this.symbol,
-            openOnly: 0,
-            orderLinkId : this.orderId_exit_1,
-            limit: 1,
-        })
-        .catch((error) => {
-            consoleLogger.error(`${this.name} getActiveOrders (exit1) failed:`, error);
-            fileLogger.error(`${this.name} getActiveOrders (exit1) failed:`, error);
-        });
+    // async doubleCheckStatus(){
+    //     const res1 = await rest_client.getActiveOrders({ 
+    //         category: 'linear',
+    //         symbol: this.symbol,
+    //         openOnly: 0,
+    //         orderLinkId : this.orderId_exit_1,
+    //         limit: 1,
+    //     })
+    //     .catch((error) => {
+    //         consoleLogger.error(`${this.name} getActiveOrders (exit1) failed:`, error);
+    //         fileLogger.error(`${this.name} getActiveOrders (exit1) failed:`, error);
+    //     });
 
-        const res2 = await rest_client.getActiveOrders({
-            category: 'linear',
-            symbol: this.symbol,
-            openOnly: 0,
-            orderLinkId : this.orderId_exit_2,
-            limit: 1,
-        })
-        .catch((error) => {
-            consoleLogger.error(`${this.name} getActiveOrders (exit2) failed:`, error);
-            fileLogger.error(`${this.name} getActiveOrders (exit2) failed:`, error);
-        });
+    //     const res2 = await rest_client.getActiveOrders({
+    //         category: 'linear',
+    //         symbol: this.symbol,
+    //         openOnly: 0,
+    //         orderLinkId : this.orderId_exit_2,
+    //         limit: 1,
+    //     })
+    //     .catch((error) => {
+    //         consoleLogger.error(`${this.name} getActiveOrders (exit2) failed:`, error);
+    //         fileLogger.error(`${this.name} getActiveOrders (exit2) failed:`, error);
+    //     });
 
-        if(res1?.result?.list?.length > 0 && res2?.result?.list?.length > 0){
-            this.isOpenOrderFilled = true
-            this.isPartialExit = false
-        }else if(res1?.result?.list?.length <= 0 && res2?.result?.list?.length > 0){
-            this.isOpenOrderFilled = true
-            this.isPartialExit = true
-        }else{
-            this.reset()
-        }
+    //     if(res1?.result?.list?.length > 0 && res2?.result?.list?.length > 0){
+    //         this.isOpenOrderFilled = true
+    //         this.isPartialExit = false
+    //     }else if(res1?.result?.list?.length <= 0 && res2?.result?.list?.length > 0){
+    //         this.isOpenOrderFilled = true
+    //         this.isPartialExit = true
+    //     }else{
+    //         this.reset()
+    //     }
 
 
-    }
+    // }
 
     setNewOrderId(){//새로운 open 주문들어갈때마다 실행필
 
         this.orderId_open = `${this.name}_open_${new Date().getTime()}`
         this.orderId_exit_1 = `${this.name}_exit1_${new Date().getTime()}`
         this.orderId_exit_2 = `${this.name}_exit2_${new Date().getTime()}`
+        this.orderId_exit_3 = `${this.name}_exit3_${new Date().getTime()}`
 
     }
 
